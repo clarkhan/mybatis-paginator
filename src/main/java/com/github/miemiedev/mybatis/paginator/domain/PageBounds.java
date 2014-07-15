0a66408fb30fffca22269e88d355d538fc501501
Khan <clarkhan@163.com>
2014-07-15 14:24:10 +0800

    调整PageBouns接口。同时调整测试代码。

--- src/main/java/com/github/miemiedev/mybatis/paginator/domain/PageBounds.java
+++ src/main/java/com/github/miemiedev/mybatis/paginator/domain/PageBounds.java
@@ -1,158 +1,188 @@
 package com.github.miemiedev.mybatis.paginator.domain;
 
-import org.apache.ibatis.session.RowBounds;
-
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.util.StringUtils;
+
 /**
- *  分页查询对象
- *  @author badqiu
- *  @author hunhun
- *  @author miemiedev
+ * 分页查询对象
+ * 
+ * @author badqiu
+ * @author hunhun
+ * @author miemiedev
+ * @author khan
  */
 public class PageBounds extends RowBounds implements Serializable {
-    private static final long serialVersionUID = -6414350656252331011L;
-    public final static int NO_PAGE = 1;
-    /** 页号 */
-    protected int page = NO_PAGE;
-    /** 分页大小 */
-    protected int limit = NO_ROW_LIMIT;
-    /** 分页排序信息 */
-    protected List<Order> orders = new ArrayList<Order>();
-    /** 结果集是否包含TotalCount */
-    protected boolean containsTotalCount;
 
-    protected Boolean asyncTotalCount;
+	private static final long serialVersionUID = -6414350656252331011L;
 
-    public PageBounds(){
-        containsTotalCount = false;
-    }
+	public final static int NO_PAGE = 1;
+	/** 页号 */
+	protected int page = NO_PAGE;
+	/** 分页大小 */
+	protected int limit = NO_ROW_LIMIT;
+	/** 分页排序信息 */
+	protected List<Order> orders = new ArrayList<Order>();
+	/** 结果集是否包含TotalCount */
+	protected boolean containsTotalCount;
+	/** 异步查询总数 */
+	protected Boolean asyncTotalCount;
 
-    public PageBounds(RowBounds rowBounds) {
-        if(rowBounds instanceof PageBounds){
-            PageBounds pageBounds = (PageBounds)rowBounds;
-            this.page = pageBounds.page;
-            this.limit = pageBounds.limit;
-            this.orders = pageBounds.orders;
-            this.containsTotalCount = pageBounds.containsTotalCount;
-            this.asyncTotalCount = pageBounds.asyncTotalCount;
-        }else{
-            this.page = (rowBounds.getOffset()/rowBounds.getLimit())+1;
-            this.limit = rowBounds.getLimit();
-        }
+	/** 附加的where 条件 */
+	private String whereClause;
+	/** 已组装好的排序语句 */
+	private String orderByClause;
 
-    }
+	/**
+	 * 默认不排序、不分页、不求总数
+	 */
+	public PageBounds() {
+	}
 
-    /**
-     * Query TOP N, default containsTotalCount = false
-     * @param limit
-     */
-    public PageBounds(int limit) {
-        this.limit = limit;
-        this.containsTotalCount = false;
-    }
+	/**
+	 * Query TOP N, default containsTotalCount = false
+	 * 
+	 * @param limit 每页大小
+	 * @see #PageBounds(int, int, boolean)
+	 */
+	public PageBounds(int limit) {
+		this(limit, NO_PAGE, false);
+	}
 
-    public PageBounds(int page, int limit) {
-        this(page, limit, new ArrayList<Order>(), true);
-    }
+	/**
+	 * 分页，默认containsTotalCount为true，即同时查询总数
+	 * 
+	 * @param limit 每页大小
+	 * @param page 页码
+	 * @see #PageBounds(int, int, boolean)
+	 */
+	public PageBounds(int limit, int page) {
+		this(limit, page, true);
+	}
 
-    public PageBounds(int page, int limit, boolean containsTotalCount) {
-        this(page, limit, new ArrayList<Order>(), containsTotalCount);
-    }
+	/**
+	 * 分页
+	 * 
+	 * @param limit 每页大小
+	 * @param page 页码
+	 * @param containsTotalCount 是否查询总数
+	 */
+	public PageBounds(int limit, int page, boolean containsTotalCount) {
+		this.page = page;
+		this.limit = limit;
+		this.containsTotalCount = containsTotalCount;
+	}
 
-    /**
-     * Just sorting, default containsTotalCount = false
-     * @param orders
-     */
-    public PageBounds(List<Order> orders) {
-        this(NO_PAGE, NO_ROW_LIMIT,orders ,false);
-    }
+	public PageBounds(RowBounds rowBounds) {
+		if (rowBounds instanceof PageBounds) {
+			PageBounds pageBounds = (PageBounds) rowBounds;
+			this.page = pageBounds.page;
+			this.limit = pageBounds.limit;
+			this.orders = pageBounds.orders;
+			this.containsTotalCount = pageBounds.containsTotalCount;
+			this.asyncTotalCount = pageBounds.asyncTotalCount;
+			this.orderByClause = pageBounds.orderByClause;
+		} else {
+			this.page = (rowBounds.getOffset() / rowBounds.getLimit()) + 1;
+			this.limit = rowBounds.getLimit();
+		}
 
-    /**
-     * Just sorting, default containsTotalCount = false
-     * @param order
-     */
-    public PageBounds(Order... order) {
-        this(NO_PAGE, NO_ROW_LIMIT,order);
-        this.containsTotalCount = false;
-    }
+	}
 
-    public PageBounds(int page, int limit, Order... order) {
-        this(page, limit, Arrays.asList(order), true);
-    }
+	public int getPage() {
+		return page;
+	}
 
-    public PageBounds(int page, int limit, List<Order> orders) {
-        this(page, limit, orders, true);
-    }
+	public void setPage(int page) {
+		this.page = page;
+	}
 
-    public PageBounds(int page, int limit, List<Order> orders, boolean containsTotalCount) {
-        this.page = page;
-        this.limit = limit;
-        this.orders = orders;
-        this.containsTotalCount = containsTotalCount;
-    }
+	@Override
+	public int getLimit() {
+		return limit;
+	}
 
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
 
-    public int getPage() {
-        return page;
-    }
+	public boolean isContainsTotalCount() {
+		return containsTotalCount;
+	}
 
-    public void setPage(int page) {
-        this.page = page;
-    }
+	public void setContainsTotalCount(boolean containsTotalCount) {
+		this.containsTotalCount = containsTotalCount;
+	}
 
-    public int getLimit() {
-        return limit;
-    }
+	public List<Order> getOrders() {
+		return orders;
+	}
 
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
 
-    public boolean isContainsTotalCount() {
-        return containsTotalCount;
-    }
+	public Boolean getAsyncTotalCount() {
+		return asyncTotalCount;
+	}
 
-    public void setContainsTotalCount(boolean containsTotalCount) {
-        this.containsTotalCount = containsTotalCount;
-    }
+	public void setAsyncTotalCount(Boolean asyncTotalCount) {
+		this.asyncTotalCount = asyncTotalCount;
+	}
 
-    public List<Order> getOrders() {
-        return orders;
-    }
+	/**
+	 * @return the orderByClause
+	 */
+	public String getOrderByClause() {
+		return orderByClause;
+	}
 
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
+	/**
+	 * @param orderByClause the orderByClause to set
+	 */
+	public void setOrderByClause(String orderByClause) {
+		this.orderByClause = orderByClause;
+	}
 
-    public Boolean getAsyncTotalCount() {
-        return asyncTotalCount;
-    }
+	/**
+	 * @return the whereClause
+	 */
+	public String getWhereClause() {
+		return whereClause;
+	}
 
-    public void setAsyncTotalCount(Boolean asyncTotalCount) {
-        this.asyncTotalCount = asyncTotalCount;
-    }
+	/**
+	 * @param whereClause the whereClause to set
+	 */
+	public void setWhereClause(String whereClause) {
+		this.whereClause = whereClause;
+	}
 
-    @Override
-    public int getOffset() {
-        if(page >= 1){
-            return (page-1) * limit;
-        }
-        return 0;
-    }
+	@Override
+	public int getOffset() {
+		if (page >= 1) {
+			return (page - 1) * limit;
+		}
+		return 0;
+	}
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("PageBounds{");
-        sb.append("page=").append(page);
-        sb.append(", limit=").append(limit);
-        sb.append(", orders=").append(orders);
-        sb.append(", containsTotalCount=").append(containsTotalCount);
-        sb.append(", asyncTotalCount=").append(asyncTotalCount);
-        sb.append('}');
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("PageBounds{");
+		sb.append("page=").append(page);
+		sb.append(", limit=").append(limit);
+		if (StringUtils.hasText(orderByClause)) {
+			sb.append(",orderByClause=").append(orderByClause);
+		} else {
+			sb.append(", orders=").append(orders);
+		}
+		sb.append(", containsTotalCount=").append(containsTotalCount);
+		sb.append(", asyncTotalCount=").append(asyncTotalCount);
+		sb.append('}');
+		return sb.toString();
+	}
+
 }
